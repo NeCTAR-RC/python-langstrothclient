a7e15ce485f3235e9e9d35ae0ca92fe3c17d91d2
Stephen Crawley <s.crawley@uq.edu.au>
2023-07-05 16:59:36 +1000

    Switch setup back to Manukaclient way
    Change-Id: I08b970ffa676d731655f6a32bb42c9d5bd2b6377

--- setup.py
+++ setup.py
@@ -2,6 +2,31 @@
 
 import setuptools
 
+from pbr.packaging import parse_requirements
+
+entry_points = {
+    'openstack.cli.extension': [
+        'outage = langstrothclient.osc.plugin'],
+    'openstack.outage.v1': [
+        'outage list = langstrothclient.osc.v1.outages:ListOutages',
+        'outage show = langstrothclient.osc.v1.outages:ShowOutage']
+}
+
 setuptools.setup(
-    setup_requires=['pbr>=2.0.0'],
-    pbr=True)
+    name='langstrothclient',
+    version='0.0.3',
+    description='Client for the Nectar Status and Outage system (Langstroth)',
+    author='Stephen Crawley',
+    author_email='stephen.crawley@ardc.edu.au',
+    url='https://github.com/NeCTAR-RC/python-langstrothclient',
+    packages=['langstrothclient'],
+    include_package_data=True,
+    setup_requires=['pbr>=3.0.0'],
+    license='Apache',
+    zip_safe=False,
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent'],
+    entry_points=entry_points,
+)
